2f377ca3d79efe5f6085b3902fa125755f13eada
vibhu1508 <142681237+vibhu1508@users.noreply.github.com>
2025-09-22 01:19:56 +0530

    major updates

--- webscraper.py
+++ webscraper.py
@@ -3,7 +3,9 @@ from selenium.webdriver.common.by import By # type: ignore
 from selenium.webdriver.support.ui import WebDriverWait # type: ignore
 from selenium.webdriver.support import expected_conditions as EC # type: ignore
 from selenium.webdriver.chrome.options import Options # type: ignore
+from selenium.webdriver.chrome.service import Service # Import Service
 from selenium.common.exceptions import TimeoutException, StaleElementReferenceException # type: ignore
+from webdriver_manager.chrome import ChromeDriverManager # Import ChromeDriverManager
 import pandas as pd
 import time
 import logging
@@ -65,7 +67,7 @@ def scrape_nse_announcements_robust(symbol="AXISBANK", limit=None):
     chrome_options.add_experimental_option('useAutomationExtension', False)
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     
     try:
