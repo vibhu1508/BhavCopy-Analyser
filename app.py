4f11392d950f86a031d1feed048f8df892b51818
vibhu1508 <142681237+vibhu1508@users.noreply.github.com>
2025-09-28 04:56:49 +0530

    check

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from stock_comparison_tab import render_stock_comparison_tab
 from new_functionality_tab import render_new_functionality_tab
 from corporate_announcements_tab import render_corporate_announcements_tab
+from bse_announcements_tab import render_bse_announcements_tab # Import BSE announcements tab
 from auth import login_form, logout_button # Import authentication functions
 
 st.set_page_config(page_title="NSE Stock Analysis Platform", layout="wide")
@@ -23,4 +24,8 @@ else:
         render_new_functionality_tab()
 
     with tab3:
-        render_corporate_announcements_tab()
+        nse_tab, bse_tab = st.tabs(["NSE Corporate Announcements", "BSE Corporate Announcements"])
+        with nse_tab:
+            render_corporate_announcements_tab()
+        with bse_tab:
+            render_bse_announcements_tab()
